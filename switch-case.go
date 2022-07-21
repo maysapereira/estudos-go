1be2a932be24fd93b0a0c0ce2aa1892c8391275f
Maysa Pereira <93635431+maysapereira@users.noreply.github.com>
2022-07-21 19:45:14 -0300

    exemplo de switch case

---
+++ switch-case.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	fmt.Println("Quando é sábado?")
+	hoje := time.Now().Weekday()
+	switch time.Saturday {
+	case hoje + 0:
+		fmt.Println("Hoje")
+	case hoje + 1:
+		fmt.Println("Amanhã.")
+	case hoje + 2:
+		fmt.Println("Daqui a dois dias")
+	default:
+		fmt.Println("Daqui a muitos dias")
+	}
+}
+}
