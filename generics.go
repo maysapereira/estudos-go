5a49c30d610d543ff27363e70e1bf7f1bebb125e
maysapereira <maysalvespereira@gmail.com~>
2022-07-25 11:31:07 -0300

    exemplo de generics

---
+++ generics.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type User[T any, B any] struct {
+	name T
+	age  B
+}
+
+func main() {
+	userTest := User[string, int64]{
+		name: "Maysa",
+		age:  22,
+	}
+
+	fmt.Println(userTest)
+}
