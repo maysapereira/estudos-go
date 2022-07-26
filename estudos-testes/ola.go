3007f5a0c9b52bc0a29f1ea2874ae170851f3858
maysapereira <maysalvespereira@gmail.com~>
2022-07-26 19:09:47 -0300

    refatora olá mundo usando testes

--- estudos-testes/ola.go
+++ estudos-testes/ola.go
@@ -2,17 +2,29 @@ package main
 
 import "fmt"
 
+const espanhol = "espanhol"
+const frances = "francês"
 const prefixoOlaPortugues = "Olá, "
+const prefixoOlaEspanhol = "Hola, "
+const prefixoOlaFrances = "Bonjour, "
 
-func Ola(nome string) string {
+func Ola(nome string, idioma string) string {
 	if nome == "" {
 		nome = "Mundo"
 	}
 
-	return prefixoOlaPortugues + nome
+	prefixo := prefixoOlaPortugues
 
+	switch idioma {
+	case frances:
+		prefixo = prefixoOlaFrances
+	case espanhol:
+		prefixo = prefixoOlaEspanhol
+	}
+
+	return prefixo + nome
 }
 
 func main() {
-	fmt.Println(Ola("Maysa"))
+	fmt.Println(Ola("mundo", ""))
 }
--- estudos-testes/ola_test.go
+++ estudos-testes/ola_test.go
@@ -9,16 +9,27 @@ func TestOla(t *testing.T) {
 			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
 		}
 	}
-
-	t.Run("diz olá para as pessoas", func(t *testing.T) {
-		resultado := Ola("Maysa")
+	t.Run("diz olá para uma pessoa", func(t *testing.T) {
+		resultado := Ola("Maysa", "")
 		esperado := "Olá, Maysa"
 		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
-	t.Run("'Mundo' como padrão para 'string' vazia", func(t *testing.T) {
-		resultado := Ola("")
+	t.Run("diz olá, mundo quando uma string vazia é passada como parâmetro", func(t *testing.T) {
+		resultado := Ola("", "")
 		esperado := "Olá, Mundo"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("diz olá em espanhol", func(t *testing.T) {
+		resultado := Ola("Maysa", espanhol)
+		esperado := "Hola, Maysa"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("diz olá em francês", func(t *testing.T) {
+		resultado := Ola("Maysa", frances)
+		esperado := "Bonjour, Maysa"
 		verificaMensagemCorreta(t, resultado, esperado)
 	})
 }
