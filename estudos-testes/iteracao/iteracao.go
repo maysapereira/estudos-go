364c9a5ba69d330c2a075b7642613f482f3fe99e
maysapereira <maysalvespereira@gmail.com~>
2022-07-27 11:11:26 -0300

    refatpra iteracao

--- estudos-testes/iteracao/iteracao.go
+++ estudos-testes/iteracao/iteracao.go
@@ -1,10 +1,12 @@
 package iteracao
 
+const quantidadeRepeticoes = 5
+
 func Repetir(caractere string) string {
 	var repeticoes string
 
-	for i := 0; i < 5; i++ {
-		repeticoes = repeticoes + caractere
+	for i := 0; i < quantidadeRepeticoes; i++ {
+		repeticoes += caractere
 	}
 
 	return repeticoes
