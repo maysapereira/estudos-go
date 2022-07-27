a2fb94c582cfe4070771b4c336a665d5e4f4368d
maysapereira <maysalvespereira@gmail.com~>
2022-07-27 14:54:18 -0300

    refatora teste com slice

--- estudos-testes/arrays-slices/soma_test.go
+++ estudos-testes/arrays-slices/soma_test.go
@@ -21,22 +21,25 @@ func TestSoma(t *testing.T) {
 
 func TesteSomaTodoOResto(t *testing.T) {
 
-	t.Run("faz as somas de alguns slices", func(t *testing.T) {
-		resultado := SomaTodoOResto([]int{1, 2}, []int{0, 9})
-		esperado := []int{2, 9}
-
+	verificaSomas := func(t *testing.T, resultado, esperado []int) {
+		t.Helper()
 		if !reflect.DeepEqual(resultado, esperado) {
 			t.Errorf("resultado %v, esperado %v", resultado, esperado)
 		}
+	}
+
+	t.Run("faz as soma do resto", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{1, 2}, []int{0, 9})
+		esperado := []int{2, 9}
+
+		verificaSomas(t, resultado, esperado)
 	})
 
 	t.Run("soma slices vazios de forma segura", func(t *testing.T) {
 		resultado := SomaTodoOResto([]int{}, []int{3, 4, 5})
 		esperado := []int{0, 9}
 
-		if !reflect.DeepEqual(resultado, esperado) {
-			t.Errorf("resultado %v, esperado %v", resultado, esperado)
-		}
+		verificaSomas(t, resultado, esperado)
 	})
 
 }
