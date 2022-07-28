79b8d9415f14f6b508562b4e42bc5efd694446cc
maysapereira <maysalvespereira@gmail.com>
2022-07-28 19:10:23 -0300

    adiciona tratamento de erros

--- estudos-testes/ponteiros-e-erros/carteira.go
+++ estudos-testes/ponteiros-e-erros/carteira.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -24,6 +27,11 @@ func (c *Carteira) Saldo() Bitcoin {
 	return c.saldo
 }
 
-func (c *Carteira) Sacar(quantidade Bitcoin) {
+func (c *Carteira) Sacar(quantidade Bitcoin) error {
+	if quantidade > c.saldo {
+		return errors.New("eita")
+	}
+
 	c.saldo -= quantidade
+	return nil
 }
--- estudos-testes/ponteiros-e-erros/carteira_test.go
+++ estudos-testes/ponteiros-e-erros/carteira_test.go
@@ -5,32 +5,36 @@ import (
 )
 
 func TestCarteira(t *testing.T) {
-	t.Run("Depositar", func(t *testing.T) {
-		carteira := Carteira{}
-
-		carteira.Depositar(Bitcoin(10))
-
+	confirmaSaldo := func(t *testing.T, carteira Carteira, esperado Bitcoin) {
+		t.Helper()
 		resultado := carteira.Saldo()
-
-		esperado := Bitcoin(10)
 
 		if resultado != esperado {
 			t.Errorf("resultado %s, esperado %s", resultado, esperado)
 		}
+	}
+
+	t.Run("Depositar", func(t *testing.T) {
+		carteira := Carteira{}
+		carteira.Depositar(Bitcoin(10))
+		confirmaSaldo(t, carteira, Bitcoin(10))
 	})
 
 	t.Run("Sacar", func(t *testing.T) {
 		carteira := Carteira{saldo: Bitcoin(20)}
-
-		carteira.Sacar(Bitcoin(10))
-
-		resultado := carteira.Saldo()
-
-		esperado := Bitcoin(10)
-
-		if resultado != esperado {
-			t.Errorf("resultado %s, esperado %s", resultado, esperado)
-		}
+		carteira.Sacar(10)
+		confirmaSaldo(t, carteira, Bitcoin(10))
 	})
 
+	t.Run("Sacar com saldo insuficiente", func(t *testing.T) {
+		saldoInicial := Bitcoin(20)
+		carteira := Carteira{saldoInicial}
+		erro := carteira.Sacar(Bitcoin(100))
+
+		confirmaSaldo(t, carteira, saldoInicial)
+
+		if erro == nil {
+			t.Error("Esperava um erro mas nenhum ocorreu")
+		}
+	})
 }
