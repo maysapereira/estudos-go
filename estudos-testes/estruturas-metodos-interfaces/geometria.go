151fa03a9dd201d2feff637cebaed42770256de4
maysapereira <maysalvespereira@gmail.com>
2022-07-28 17:16:00 -0300

    testando função de geometria

---
+++ estudos-testes/estruturas-metodos-interfaces/geometria.go
@@ -0,0 +1,10 @@
+package main
+
+func Perimetro(largura float64, altura float64) float64 {
+	return 2 * (largura + altura)
+}
+
+func Area(largura float64, altura float64) float64 {
+	return largura + altura
+
+}
