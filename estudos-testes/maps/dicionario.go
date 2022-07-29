b67b00ae38f399749c89a0bcbf5c8c23752ca51f
maysapereira <maysalvespereira@gmail.com>
2022-07-29 09:46:41 -0300

    adiciona tratamento de erros para quando uma palavra não for encontrada

--- estudos-testes/maps/dicionario.go
+++ estudos-testes/maps/dicionario.go
@@ -1,7 +1,15 @@
 package main
 
+import "errors"
+
 type Dicionario map[string]string
 
-func (d Dicionario) Busca(palavra string) string {
-	return d[palavra]
+func (d Dicionario) Busca(palavra string) (string, error) {
+	definicao, existe := d[palavra]
+
+	if !existe {
+		return "", errors.New("não foi possível encontrar a palavra que você procura")
+	}
+
+	return definicao, nil
 }
--- estudos-testes/maps/dicionario_test.go
+++ estudos-testes/maps/dicionario_test.go
@@ -5,10 +5,20 @@ import "testing"
 func TestBusca(t *testing.T) {
 	dicionario := Dicionario{"teste": "isso é apenas um teste"}
 
-	resultado := dicionario.Busca("teste")
-	esperado := "isso é apenas um teste"
+	t.Run("palavra conhecida", func(t *testing.T) {
+		resultado, _ := dicionario.Busca("teste")
+		esperado := "isso é apenas um teste"
 
-	comparaStrings(t, resultado, esperado)
+		comparaStrings(t, resultado, esperado)
+	})
+
+	t.Run("palavra desconhecida", func(t *testing.T) {
+		_, err := dicionario.Busca("desconhecida")
+
+		if err == nil {
+			t.Fatal("é esperado que um erro seja obtido")
+		}
+	})
 
 }
 
