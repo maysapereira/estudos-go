902f67379ed4a250a7100ffa55abe483ca1527a4
maysapereira <maysalvespereira@gmail.com>
2022-07-31 21:08:12 -0300

    testando concorrencia

--- estudos-testes/concorrencia/concorrencia.go
+++ estudos-testes/concorrencia/concorrencia.go
@@ -1,13 +1,19 @@
 package concorrencia
 
+import "time"
+
 type VerificadorWebsite func(string) bool
 
 func VerificaWebsites(vw VerificadorWebsite, urls []string) map[string]bool {
 	resultados := make(map[string]bool)
 
 	for _, url := range urls {
-		resultados[url] = vw(url)
+		go func(u string) {
+			resultados[u] = vw(u)
+		}(url)
 	}
+
+	time.Sleep(2 * time.Second)
 
 	return resultados
 }
--- estudos-testes/concorrencia/concorrencia_test.go
+++ estudos-testes/concorrencia/concorrencia_test.go
@@ -1,34 +1,24 @@
 package concorrencia
 
 import (
-	"reflect"
 	"testing"
+	"time"
 )
 
-func mockVerificadorWebsite(url string) bool {
-	if url == "waat://furhurterwe.geds" {
-		return false
-	}
-
+func slowStubVerificadorWebsite(_ string) bool {
+	time.Sleep(20 * time.Millisecond)
 	return true
 }
 
-func TestVerificaWebsites(t *testing.T) {
-	websites := []string{
-		"http://google.com",
-		"http://blog.gypsydave5.com",
-		"waat://furhurterwe.geds",
+func BenchmarkVerificaWebsites(b *testing.B) {
+	urls := make([]string, 100)
+
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "uma url"
 	}
 
-	esperado := map[string]bool{
-		"http://google.com":          true,
-		"http://blog.gypsydave5.com": true,
-		"waat://furhurterwe.geds":    false,
+	for i := 0; i < b.N; i++ {
+		VerificaWebsites(slowStubVerificadorWebsite, urls)
 	}
 
-	resultado := VerificaWebsites(mockVerificadorWebsite, websites)
-
-	if !reflect.DeepEqual(esperado, resultado) {
-		t.Fatalf("esperado %v, resultado %v", esperado, resultado)
-	}
 }
