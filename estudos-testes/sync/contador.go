62c43460581180d2e5434d91e9c3f714271b241d
maysapereira <maysalvespereira@gmail.com>
2022-08-01 09:35:46 -0300

    refatora sync

--- estudos-testes/sync/contador.go
+++ estudos-testes/sync/contador.go
@@ -1,10 +1,19 @@
 package main
 
+import "sync"
+
 type Contador struct {
+	mu    sync.Mutex
 	valor int
 }
 
+func NovoContador() *Contador {
+	return &Contador{}
+}
+
 func (c *Contador) Incrementa() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.valor++
 }
 
--- estudos-testes/sync/contador_test.go
+++ estudos-testes/sync/contador_test.go
@@ -1,22 +1,42 @@
 package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestContador(t *testing.T) {
-	t.Run("incrementar o contador 3 vezes resultado no valor 3", func(t *testing.T) {
-		contador := Contador{}
+	t.Run("incrementar o contador 3 vezes o deixa com valor 3", func(t *testing.T) {
+		contador := NovoContador()
 		contador.Incrementa()
 		contador.Incrementa()
 		contador.Incrementa()
 
 		verificaContador(t, contador, 3)
+	})
 
+	t.Run("roda concorrentemente em segurança", func(t *testing.T) {
+		contagemEsperada := 1000
+		contador := NovoContador()
+
+		var wg sync.WaitGroup
+		wg.Add(contagemEsperada)
+
+		for i := 0; i < contagemEsperada; i++ {
+			go func(w *sync.WaitGroup) {
+				contador.Incrementa()
+				w.Done()
+			}(&wg)
+		}
+		wg.Wait()
+
+		verificaContador(t, contador, contagemEsperada)
 	})
 }
 
-func verificaContador(t *testing.T, resultado Contador, esperado int) {
+func verificaContador(t *testing.T, resultado *Contador, esperado int) {
 	t.Helper()
 	if resultado.Valor() != esperado {
-		t.Errorf("resultado %d, esperado %d", resultado.Valor(), 3)
+		t.Errorf("resultado %d, esperado %d", resultado.Valor(), esperado)
 	}
 }
