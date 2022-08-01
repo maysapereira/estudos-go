130f3740b2348e2474f011e7d4a3c5c7b62af8f4
maysapereira <maysalvespereira@gmail.com>
2022-07-31 21:30:23 -0300

    refatora teste de concorrencia

--- estudos-testes/concorrencia/concorrencia.go
+++ estudos-testes/concorrencia/concorrencia.go
@@ -1,19 +1,25 @@
 package concorrencia
 
-import "time"
-
 type VerificadorWebsite func(string) bool
+type resultado struct {
+	string
+	bool
+}
 
 func VerificaWebsites(vw VerificadorWebsite, urls []string) map[string]bool {
 	resultados := make(map[string]bool)
+	canalResultado := make(chan resultado)
 
 	for _, url := range urls {
 		go func(u string) {
-			resultados[u] = vw(u)
+			canalResultado <- resultado{u, vw(u)}
 		}(url)
 	}
 
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		resultado := <-canalResultado
+		resultados[resultado.string] = resultado.bool
+	}
 
 	return resultados
 }
