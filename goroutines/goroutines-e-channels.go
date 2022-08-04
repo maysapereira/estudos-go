78041889633835cacdf57ffbd7bbe88b38ee3a79
maysapereira <maysalvespereira@gmail.com>
2022-08-03 22:56:49 -0300

    exemplo de gourotine usando channel

--- goroutines/goroutines-e-channels.go
+++ goroutines/goroutines-e-channels.go
@@ -5,23 +5,33 @@ import (
 	"time"
 )
 
-func numeros() {
+func numeros(done chan<- bool) {
 	for i := 0; i < 10; i++ {
 		fmt.Printf("%d ", i)
 		time.Sleep(time.Millisecond * 150)
 	}
+
+	done <- true
 }
 
-func letras() {
+func letras(done chan<- bool) {
 	for l := 'a'; l < 'j'; l++ {
 		fmt.Printf("%c ", l)
 		time.Sleep(time.Millisecond * 230)
 	}
+
+	done <- true
 }
 
 func main() {
-	go numeros()
-	go letras()
-	time.Sleep(5 * time.Second)
+	cn := make(chan bool)
+	cl := make(chan bool)
+
+	go numeros(cn)
+	go letras(cl)
+
+	<-cn
+	<-cl
+
 	fmt.Println("Fim da execução!")
 }
