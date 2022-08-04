2ea653053bb85ca7c8c26dd3f76625733305a694
maysapereira <maysalvespereira@gmail.com>
2022-08-04 14:45:33 -0300

    organiza e cria metodo pro square

--- metodos/perimeter.go
+++ metodos/perimeter.go
@@ -2,11 +2,9 @@ package main
 
 import "fmt"
 
-type triangle struct {
-	size int
-}
+//Triangle
 
-type square struct {
+type triangle struct {
 	size int
 }
 
@@ -19,20 +17,35 @@ func (t triangle) perimeter() int {
 	return t.size * 3
 }
 
-func (s square) perimeter() int {
-	return s.size * 4
-}
-
 func (t coloredTriangle) perimeter() int {
 	return t.triangle.perimeter()
 }
 
-func main() {
-	t := coloredTriangle{triangle{3}, "blue"}
-	s := square{4}
+//Square
+type square struct {
+	size int
+}
 
+type coloredSquare struct {
+	square
+	color string
+}
+
+func (s square) perimeter() int {
+	return s.size * 4
+}
+
+func (t coloredSquare) perimeter() int {
+	return t.square.perimeter()
+}
+
+func main() {
+
+	t := coloredTriangle{triangle{3}, "blue"}
 	fmt.Println("Perimeter (triangle):", t.perimeter())
 	fmt.Println("Size:", t.size)
 
+	s := coloredSquare{square{4}, "red"}
 	fmt.Println("Perimeter (square):", s.perimeter())
+	fmt.Println("Size:", s.size)
 }
