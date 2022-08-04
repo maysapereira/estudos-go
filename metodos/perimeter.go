2f727e6d448253ea47667198d4a17dbfa73b8e84
maysapereira <maysalvespereira@gmail.com>
2022-08-04 14:38:15 -0300

    exemplo de métodos (inserindo métodos)

--- metodos/perimeter.go
+++ metodos/perimeter.go
@@ -10,6 +10,11 @@ type square struct {
 	size int
 }
 
+type coloredTriangle struct {
+	triangle
+	color string
+}
+
 func (t triangle) perimeter() int {
 	return t.size * 3
 }
@@ -18,10 +23,16 @@ func (s square) perimeter() int {
 	return s.size * 4
 }
 
+func (t coloredTriangle) perimeter() int {
+	return t.triangle.perimeter()
+}
+
 func main() {
-	t := triangle{3}
+	t := coloredTriangle{triangle{3}, "blue"}
 	s := square{4}
 
 	fmt.Println("Perimeter (triangle):", t.perimeter())
+	fmt.Println("Size:", t.size)
+
 	fmt.Println("Perimeter (square):", s.perimeter())
 }
