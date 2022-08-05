bf42eeb2ca18ad209f11f54a3c64d3c6e9ce7f3e
maysapereira <maysalvespereira@gmail.com>
2022-08-04 23:32:49 -0300

    adiciona api store

---
+++ api-store/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"store"
+)
+
+func main() {
+	bruce, _ := store.CreateEmployee("Bruce", "Lee", 500)
+	fmt.Println(bruce.CheckCredits())
+	credits, err := bruce.AddCredits(250)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("New Credits Balance =", credits)
+	}
+
+	_, err = bruce.RemoveCredits(2500)
+
+	if err != nil {
+		fmt.Println("Can't withdraw or overdrawn!", err)
+	}
+
+	bruce.ChangeName("Mark")
+
+	fmt.Println(bruce)
+}
