5ef8b5537f88d3ee34f8caffd5a6fb02f1cce3e0
maysapereira <maysalvespereira@gmail.com>
2022-08-05 09:47:07 -0300

    exemplo de logs

---
+++ logs/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.OpenFile("info.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	log.SetOutput(file)
+	log.Print("Hey, I'm a log!")
+}
